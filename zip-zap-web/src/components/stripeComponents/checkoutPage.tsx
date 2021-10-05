93a897b6b4c1f2b4a392b2d26e92fead76ed2640
victoria-skylan <50216078+victoria-skylan@users.noreply.github.com>
2021-10-05 13:09:07 -0600

    updating add a credit card
    making sure it has bootstrap

--- zip-zap-web/src/components/stripeComponents/checkoutPage.tsx
+++ zip-zap-web/src/components/stripeComponents/checkoutPage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import appSettings from "../../appSettings.json";
 import { loadStripe } from "@stripe/stripe-js";
+import { Row, Col,} from "react-bootstrap";
 import { Link, RouteComponentProps } from "react-router-dom";
 import StripeWrapper from "./stripeWrapper";
 import StripeFullCardForm from "./stripeFullCardForm";
+import AdminItemRow from "../basicComponents/adminComponents/adminItemRow";
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
@@ -20,34 +22,36 @@ function CheckoutPage({ match, location }: RouteComponentProps) {
   let checkoutType = query.get("checkoutType");
 
   return (
-    <section className={`column center-column full-height`}>
-      <header className={`column center page-header page-header-black`}>
-        <h1>Check Out</h1>
-      </header>
-
+    <Col>
+    <Row>
+      <Col className="page-header justify-content-center ">
+        <h3>Add a card</h3>
+      </Col>
+    </Row>
+    <Row>
       {redirectURL ? (
-        <section className={`row width-90`}>
+        <Col sm="4">
           <Link to={redirectURL} className={`back-link`}>
             Back to Previous Page
           </Link>
-        </section>
+        </Col>
       ) : null}
+      </Row>
 
       {/* holds 4 Containers */}
-      <main
-        className={` column center-column main-section main-section-white-bg`}
+      <Row
+        className={`main-section-white-bg m-3`}
       >
+        <Col>
         {/* <div className={`event-dashboard-sub-title primary-green-header`}>
           <span>{completedEvent ? completedEvent.name : "Event"} Details</span>
         </div> */}
 
         {/* checkout confirmation  */}
-        <section className={`column center-row event-dashboard-full-column `}>
+        {/* <section className={`event-dashboard-full-column`}> */}
           {/* <div className={`event-dashboard-sub-title primary-black-header`}>
             <span>Checkout</span>
           </div> */}
-          <br></br>
-
           <div className={`row center-row full-width`}>
             {checkoutType === "payment" ? (
               <div
@@ -64,23 +68,24 @@ function CheckoutPage({ match, location }: RouteComponentProps) {
                 </div>
               </div>
             ) : null}
-            <div
-              className={`event-dashboard-half-column column left-align-column column-grey-bg`}
-            >
-              <div className={`event-dashboard-sub-title primary-blue-header`}>
+              <Row className={`event-dashboard-sub-title primary-blue-header`}>
                 <span>Card Information</span>
-              </div>
+              </Row>
+            <Row
+              className={`event-dashboard-half-column column left-align-column column-grey-bg pt-3`}
+            >
               <StripeWrapper>
                 <StripeFullCardForm
                   buttonType={checkoutType ? checkoutType : ""}
                   redirectURL={redirectURL ? redirectURL : ""}
                 />
               </StripeWrapper>
-            </div>
+            </Row>
           </div>
-        </section>
-      </main>
-    </section>
+        {/* </section> */}
+        </Col>
+      </Row>
+    </Col>
   );
 }
 
