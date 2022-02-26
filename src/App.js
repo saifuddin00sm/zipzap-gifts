153522e9a2571dc40be0bb88c4707f193e17c1a0
Amelia Mendicino <amelia.mendicino@gmail.com>
2022-02-26 15:07:04 -0700

    Add Auth
    Added Authentication to the app. We'll need to re-do it later to get
    people to sign up with their first and last name and company and
    we're going to need to Zip Zap brand it annnnnd stuff.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Amplify, { API, graphqlOperation } from "aws-amplify";
 import { createTodo } from "./graphql/mutations";
 import { listTodos } from "./graphql/queries";
 
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
@@ -44,30 +47,39 @@ const App = () => {
   }
 
   return (
-    <div style={styles.container}>
-      <h2>Amplify Todos</h2>
-      <input
-        onChange={(event) => setInput("name", event.target.value)}
-        style={styles.input}
-        value={formState.name}
-        placeholder="Name"
-      />
-      <input
-        onChange={(event) => setInput("description", event.target.value)}
-        style={styles.input}
-        value={formState.description}
-        placeholder="Description"
-      />
-      <button style={styles.button} onClick={addTodo}>
-        Create Todo
-      </button>
-      {todos.map((todo, index) => (
-        <div key={todo.id ? todo.id : index} style={styles.todo}>
-          <p style={styles.todoName}>{todo.name}</p>
-          <p style={styles.todoDescription}>{todo.description}</p>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div style={styles.container}>
+          <h1>Hello {user.username}</h1>
+          <button style={styles.button} onClick={signOut}>
+            Sign out
+          </button>
+          <br />
+          <h2>Amplify Todos</h2>
+          <input
+            onChange={(event) => setInput("name", event.target.value)}
+            style={styles.input}
+            value={formState.name}
+            placeholder="Name"
+          />
+          <input
+            onChange={(event) => setInput("description", event.target.value)}
+            style={styles.input}
+            value={formState.description}
+            placeholder="Description"
+          />
+          <button style={styles.button} onClick={addTodo}>
+            Create Todo
+          </button>
+          {todos.map((todo, index) => (
+            <div key={todo.id ? todo.id : index} style={styles.todo}>
+              <p style={styles.todoName}>{todo.name}</p>
+              <p style={styles.todoDescription}>{todo.description}</p>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+    </Authenticator>
   );
 };
 
