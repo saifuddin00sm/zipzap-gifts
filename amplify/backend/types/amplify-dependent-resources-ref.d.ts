c7b147d0124a587845b5f1d0acb6a7da9e9a0a0f
Amelia Mendicino <amelia.mendicino@gmail.com>
2022-03-04 01:24:56 -0700

    Update API Auth Settings
    Looks like we also needed to update the api's auth settings to get
    the user pool stuff in the correct spot. Ran the command amplify api update
    and was able to walk through the command line instructions to get that
    automatically set up properly.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,7 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
     "api": {
         "zipzap": {
-            "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
