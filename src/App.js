dc6f3b95b3c5c80f540bb599d056c742524b003a
Amelia Mendicino <amelia.mendicino@gmail.com>
2022-03-21 19:01:29 -0600

    Add LoginPage
    Refactored the App.js file into a LoginPage component so that we can
    keep it really clean. It will only render the login stuff if the user
    is currently not logged in.

--- src/App.js
+++ src/App.js
@@ -2,63 +2,31 @@
 import React from "react";
 import { Amplify } from "aws-amplify";
 import { Outlet } from "react-router-dom";
+import { Authenticator } from "@aws-amplify/ui-react";
 import NavigationMenu from "./components/NavigationMenu";
-import "./App.css";
-import useAuth from "./hooks/useAuth";
+import LoginPage from "./components/LoginPage";
 
-import { Authenticator, Grid, View, Image } from "@aws-amplify/ui-react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@aws-amplify/ui-react/styles.css";
 import "./App.css";
+
 import awsExports from "./aws-exports";
-import "bootstrap/dist/css/bootstrap.min.css";
 Amplify.configure(awsExports);
 
 const App = () => {
-  const { currentUser } = useAuth();
   return (
-    <>
-      {currentUser ? (
-        <Authenticator>
-          {({ signOut, user }) => (
-            <>
-              <NavigationMenu signOut={signOut} user={user} />
-              <div style={styles.container}>
-                <Outlet context={[user]} />
-              </div>
-            </>
-          )}
-        </Authenticator>
-      ) : (
-        <Grid
-          templateColumns="1fr 1fr"
-          templateRows="10rem 10rem"
-          gap="var(--amplify-space-small)"
-        >
-          <View className="sidebar-image">
-            <Image
-              alt="Zip Zap logo"
-              src="https://s3.amazonaws.com/content.zipzapgifts.com/login-photo.jpg"
-              className="sign-in-image"
-            />
-          </View>
-          <View className="authentication-side">
-            <h1 className="authentication-title">
-              An Automated Gift Giving Platform Built for Businesses
-            </h1>
-            <Authenticator>
-              {({ signOut, user }) => (
-                <>
-                  <NavigationMenu signOut={signOut} user={user} />
-                  <div style={styles.container}>
-                    <Outlet context={[user]} />
-                  </div>
-                </>
-              )}
-            </Authenticator>
-          </View>
-        </Grid>
-      )}
-    </>
+    <LoginPage>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <>
+            <NavigationMenu signOut={signOut} user={user} />
+            <div style={styles.container}>
+              <Outlet context={[user]} />
+            </div>
+          </>
+        )}
+      </Authenticator>
+    </LoginPage>
   );
 };
 
