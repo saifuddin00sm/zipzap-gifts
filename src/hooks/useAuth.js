1b30f7061ddbcf71cbd4fe554eea6775bb896d31
Amelia Mendicino <amelia.mendicino@gmail.com>
2022-03-23 09:51:56 -0600

    Fix useAuth Whitespace
    It looks like the useAuth is supposed to be 2 spaces and had accidentally
    been set to with width 4. Oops. Hahaha....fixing it so that the PR
    is more accurate.

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -3,32 +3,32 @@ import { Hub } from "@aws-amplify/core";
 import { useEffect, useState } from "react";
 
 const getCurrentUser = async () => {
-    try {
-        return await Auth.currentAuthenticatedUser();
-    } catch {
-        // currentAuthenticatedUser throws an Error if not signed in
-        return null;
-    }
+  try {
+    return await Auth.currentAuthenticatedUser();
+  } catch {
+    // currentAuthenticatedUser throws an Error if not signed in
+    return null;
+  }
 };
 
 const useAuth = () => {
-    const [currentUser, setCurrentUser] = useState(null);
-    useEffect(() => {
-        const updateUser = async () => {
-            setCurrentUser(await getCurrentUser());
-        };
-        Hub.listen("auth", updateUser); // listen for login/signup events
-        updateUser(); // check manually the first time because we won't get a Hub event
-        return () => Hub.remove("auth", updateUser);
-    }, []);
-
-    const signIn = () => Auth.federatedSignIn();
-
-    const signOut = () => {
-        Auth.signOut();
-        window.location.href = "/";
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    const updateUser = async () => {
+      setCurrentUser(await getCurrentUser());
     };
-    return { currentUser, signIn, signOut };
+    Hub.listen("auth", updateUser); // listen for login/signup events
+    updateUser(); // check manually the first time because we won't get a Hub event
+    return () => Hub.remove("auth", updateUser);
+  }, []);
+
+  const signIn = () => Auth.federatedSignIn();
+
+  const signOut = () => {
+    Auth.signOut();
+    window.location.href = "/";
+  };
+  return { currentUser, signIn, signOut };
 };
 export default useAuth;
 export { getCurrentUser };
