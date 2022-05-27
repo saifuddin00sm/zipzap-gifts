ec4d2ebe94d3a8226f72c51e2c202fadcc8b50c6
Saif uddin <dev.saifuddin@gmail.com>
2022-05-27 00:32:18 -0600

    updated zipit dummy data with Krista's data.

--- src/components/GiftCards.js
+++ src/components/GiftCards.js
@@ -43,31 +43,64 @@ const zipIt = [
   {
     active: null,
     category: "zipIt",
-    description: "send a virtual gift card to thread wallets",
+    description: "Send a virtual gift card to thread wallets!",
     id: "sgab45f9-1x26-777-8edla-b73c6dcf5a26",
     items: { items: [] },
-    name: "Small Baby Basket",
-    pictures: { items: [] },
+    name: "Thread Wallets",
+    pictures: {
+      items: [
+        {
+          src: "https://cdn.shopify.com/s/files/1/1030/4291/files/all-mobile_1200x.jpg?v=7400654666644309467",
+          alt: "thumbnail",
+        },
+        {
+          src: "https://cdn.shopify.com/s/files/1/1030/4291/files/all-mobile_1200x.jpg?v=7400654666644309467",
+          alt: "Thread Wallets",
+        },
+      ],
+    },
     price: "25",
   },
   {
     active: null,
     category: "zipIt",
-    description: "send a virtual gift card to thread wallets",
+    description: "Send a virtual gift card to pillow cube!",
     id: "deak45f9-1x26-8888-8edca-b73c6dcf5a26",
     items: { items: [] },
-    name: "Small anniversary gift",
-    pictures: { items: [] },
+    name: "Pillow Cube",
+    pictures: {
+      items: [
+        {
+          src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKKK8P3i4hIasdgR4nMOKhFp1uJH6SF2I9vQ&usqp=CAU",
+          alt: "thumbnail",
+        },
+        {
+          src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKKK8P3i4hIasdgR4nMOKhFp1uJH6SF2I9vQ&usqp=CAU",
+          alt: "Pillow Cube",
+        },
+      ],
+    },
     price: "995",
   },
   {
     active: null,
     category: "zipIt",
-    description: "send a virtual gift card to thread wallets",
+    description: "Send a virtual gift card to thread wallets!",
     id: "deab45y9-1x26-9999-8edla-b73c6dcf5a26",
     items: { items: [] },
-    name: "Valentine's day gift",
-    pictures: { items: [] },
+    name: "Golden Coil",
+    pictures: {
+      items: [
+        {
+          src: "https://www.goldencoil.com/assets/information_notebook-cover-f962599d3e63ca3bd3d417b073edc3b620c35fe14188f12a0c4e47430367f4b2.jpg",
+          alt: "thumbnail",
+        },
+        {
+          src: "https://www.goldencoil.com/assets/information_notebook-cover-f962599d3e63ca3bd3d417b073edc3b620c35fe14188f12a0c4e47430367f4b2.jpg",
+          alt: "Golden Coil",
+        },
+      ],
+    },
     price: "50",
   },
 ];
