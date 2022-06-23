dd95f6e103dd9d021c33b33a5e71a8ea49e3b668
Amelia Mendicino <amelia.mendicino@gmail.com>
2022-06-23 00:42:38 -0600

    Remove Delete Dialog Styling
    Getting rid of the styling on the DeleteModal's Dialog because it didn't
    work on mobile (squished it weird) and the default works well for
    our needs at this point in time.

--- src/components/Recipients/DeleteModal.js
+++ src/components/Recipients/DeleteModal.js
@@ -10,7 +10,7 @@ import DialogTitle from "@mui/material/DialogTitle";
 
 const DeleteModal = ({ open, setOpen, recipientId }) => {
   const { removeRecipient } = useRecipients();
-  let navigate = useNavigate();
+  const navigate = useNavigate();
   const onClose = () => {
     setOpen(false);
   };
@@ -24,7 +24,6 @@ const DeleteModal = ({ open, setOpen, recipientId }) => {
   return (
     <>
       <Dialog
-        style={style}
         open={open}
         onClose={onClose}
         aria-labelledby="alert-dialog-title"
@@ -51,47 +50,3 @@ const DeleteModal = ({ open, setOpen, recipientId }) => {
   );
 };
 export default DeleteModal;
-
-const style = {
-  width: "40%",
-  borderRadius: "9px",
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  "& .closeBtn": {
-    display: "flex",
-    justifyContent: "flex-end",
-    padding: "13px 13px 0px 0px",
-    "& .mainBtn": {
-      background: "#343436",
-      display: "flex",
-      justifyContent: "center",
-      borderRadius: "100%",
-      alignItems: "center",
-      width: "30px",
-      cursor: "pointer",
-      height: "30px",
-    },
-  },
-
-  "& .successHead": {
-    fontStyle: "normal",
-    fontWeight: 600,
-    fontSize: "30px",
-    lineHeight: "45px",
-    textTransform: "capitalize",
-    color: "#343436",
-    marginBottom: "15px",
-  },
-  "& .subHead": {
-    fontStyle: "normal",
-    fontWeight: 300,
-    fontSize: "20px",
-    lineHeight: "30px",
-    color: "#000000",
-    marginBottom: "20px",
-  },
-};
