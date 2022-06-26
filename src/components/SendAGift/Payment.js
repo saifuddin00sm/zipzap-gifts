966baae613bd93854990543df0feb57d6cc2838f
Amelia Mendicino <amelia.mendicino@gmail.com>
2022-06-25 21:55:58 -0600

    Display Credit Cards
    Displaying the Credit Cards returned from the /payments endpoint on the
    Payment component. It appears that the classes on the Checkout are applying
    to the Payment.js, or...something....I don't want to worry about that right
    now. It looks good right now. We can fix it later.

--- src/components/SendAGift/Payment.js
+++ src/components/SendAGift/Payment.js
@@ -4,6 +4,9 @@ import { Auth } from "@aws-amplify/auth";
 import { Elements, PaymentElement } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import usePayment from "../../hooks/usePayment";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import CircularProgress from "@mui/material/CircularProgress";
 import Alert from "@mui/material/Alert";
 
@@ -31,9 +34,71 @@ const Submit = ({ setErrorMessage }) => {
   return null;
 };
 
+const CreditCards = ({ cards }) => {
+  return (
+    <Box>
+      <Typography variant="h6">Choose Credit Card</Typography>
+      <Box className="credit_cards">
+        <Box className="inner_card">
+          {cards.map(({ isSelected, id, name, last4, exp_month, exp_year }) => (
+            <Box key={id} className="credit_card">
+              <Box
+                className="card_top"
+                sx={{ background: isSelected ? "#F1F1F1" : "#C5D6E2" }}
+              >
+                <Typography
+                  variant="h6"
+                  sx={{
+                    textAlign: "right",
+                    textTransform: "capitalize",
+                  }}
+                >
+                  Credit Card
+                </Typography>
+              </Box>
+              <Box
+                className="card_bottom"
+                sx={{ background: isSelected ? "#DEDEDE" : "#ABC4D6" }}
+              >
+                <Typography variant="h6">Ending In {last4}</Typography>
+                <Typography variant="body2">
+                  Exp: {exp_month}/{exp_year}
+                </Typography>
+                <Typography variant="body2">{name}</Typography>
+              </Box>
+            </Box>
+          ))}
+        </Box>
+        <Button variant="outlined">Add New Card</Button>
+      </Box>
+    </Box>
+  );
+};
+
 const Payment = ({ callSubmit }) => {
+  const [loadingCards, setLoadingCards] = useState(true);
+  const [cards, setCards] = useState([]);
   const [clientSecret, setClientSecret] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    const getSecret = async () => {
+      try {
+        const currentSession = await Auth.currentSession();
+        const accessToken = currentSession.getAccessToken();
+        const token = accessToken.getJwtToken();
+        const response = await API.post("stripe", "/payments", {
+          body: { token },
+        });
+        setCards(Object.values(response));
+      } catch (error) {
+        // Ignoring the error and falling back to having the user submit new payment information
+      }
+      setLoadingCards(false);
+    };
+
+    getSecret();
+  }, []);
 
   useEffect(() => {
     const getSecret = async () => {
@@ -56,6 +121,14 @@ const Payment = ({ callSubmit }) => {
 
   if (errorMessage) {
     return <Alert severity="error">{errorMessage}</Alert>;
+  }
+
+  if (loadingCards) {
+    return <CircularProgress />;
+  }
+
+  if (cards.length > 0) {
+    return <CreditCards cards={cards} />;
   }
 
   if (!clientSecret) {
