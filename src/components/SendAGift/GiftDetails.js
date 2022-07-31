07e56d133b26cd5704c2fbbf1d08356e7fce94f9
akichan89 <growingupcollegestyle@gmail.com>
2022-07-31 03:52:11 -0600

    added MUI min and max date values for to and from dates to help with date validation

--- src/components/SendAGift/GiftDetails.js
+++ src/components/SendAGift/GiftDetails.js
@@ -121,6 +121,7 @@ const GiftDetails = ({
                   <DatePicker
                     value={from}
                     inputFormat="MM/dd/yyyy"
+                    minDate={from}
                     onChange={(value) => setInput("from", value)}
                     renderInput={(params) => (
                       <TextField variant="standard" {...params} />
@@ -132,9 +133,15 @@ const GiftDetails = ({
                   <DatePicker
                     value={to}
                     inputFormat="MM/dd/yyyy"
+                    minDate={from}
+                    maxDate={new Date().setDate(395)}
                     onChange={(value) => setInput("to", value)}
                     renderInput={(params) => (
-                      <TextField variant="standard" {...params} />
+                      <TextField
+                        variant="standard"
+                        {...params}
+                        helperText="You can only plan Gifts for one year in advance. Don't worry, we will remind you to schedule these out again next year!"
+                      />
                     )}
                   />
                   <Typography sx={{ fontSize: "1.1rem" }} variant="h6">
