988de1a6cb356075d11d92d5afc0add1c9d1a3ba
Amelia Mendicino <amelia.mendicino@gmail.com>
2022-08-21 17:45:58 -0600

    Capture Email Errors
    Wrapping the whole checkoutemail function into a try-catch so that if we
    ever have any errors, we log them and then return as normal. This is because
    it appears that the function will try running again and again and again if
    there's an error, and we'd rather just log it and move on...

--- amplify/backend/function/checkoutemail/src/index.js
+++ amplify/backend/function/checkoutemail/src/index.js
@@ -20,48 +20,53 @@ import { getRecipients, getGiftName } from "./graphql.js";
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 export const handler = async (event) => {
-  // The event stream may have batched multiple records, so loop through them all
-  for await (const record of event.Records) {
-    const { eventName, dynamodb: { NewImage = {} } = {} } = record;
-    const {
-      giftID,
-      createdBy,
-      recipientIDs,
-      toDate,
-      fromDate,
-      orderType,
-      totalPrice,
-    } = NewImage;
+  try {
+    // The event stream may have batched multiple records, so loop through them all
+    for await (const record of event.Records) {
+      const { eventName, dynamodb: { NewImage = {} } = {} } = record;
+      const {
+        giftID,
+        createdBy,
+        recipientIDs,
+        toDate,
+        fromDate,
+        orderType,
+        totalPrice,
+      } = NewImage;
 
-    const recipients = await getRecipients(recipientIDs.L.map((r) => r.S));
-    const giftName = await getGiftName(giftID.S);
+      const recipients = await getRecipients(recipientIDs.L.map((r) => r.S));
+      const giftName = await getGiftName(giftID.S);
 
-    let date = new Date(toDate.S).toLocaleDateString("en-US");
-    if (orderType.S === "RECURRING") {
-      date = new Date(fromDate.S).toLocaleDateString("en-US") + " - " + date;
+      let date = new Date(toDate.S).toLocaleDateString("en-US");
+      if (orderType.S === "RECURRING") {
+        date = new Date(fromDate.S).toLocaleDateString("en-US") + " - " + date;
+      }
+
+      let email;
+      if (eventName === "INSERT") {
+        email = adminNew({
+          emailAddress: createdBy.S,
+          giftName,
+          recipients,
+          date,
+          price: totalPrice.S,
+        });
+      } else if (eventName === "MODIFY") {
+        // TODO: Calculate differences and stuff...
+        email = adminNew({
+          emailAddress: createdBy.S,
+          giftName,
+          recipients,
+          date,
+          price: totalPrice.S,
+        });
+      }
+
+      console.log(email);
     }
-
-    let email;
-    if (eventName === "INSERT") {
-      email = adminNew({
-        emailAddress: createdBy.S,
-        giftName,
-        recipients,
-        date,
-        price: totalPrice.S,
-      });
-    } else if (eventName === "MODIFY") {
-      // TODO: Calculate differences and stuff...
-      email = adminNew({
-        emailAddress: createdBy.S,
-        giftName,
-        recipients,
-        date,
-        price: totalPrice.S,
-      });
-    }
-
-    console.log(email);
+  } catch (err) {
+    // Wrapped the whole function in a try-catch so that it doesn't re-run if it fails
+    console.log(err);
   }
   return { status: "done" };
 };
