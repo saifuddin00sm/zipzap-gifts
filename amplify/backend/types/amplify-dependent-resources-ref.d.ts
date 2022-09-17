119bbc8eaa48bce7ffc7116357a18170cfb42876
Amelia Mendicino <amelia.mendicino@gmail.com>
2022-09-17 11:54:18 -0600

    Add favorites Lambda
    Adding a new lambda function named "favorites" to be invoked by a Google
    Form submission to save user favorites to the GraphQL database.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -43,6 +43,12 @@ export type AmplifyDependentResourcesAttributes = {
             "Arn": "string",
             "Region": "string",
             "LambdaExecutionRole": "string"
+        },
+        "favorites": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
         }
     }
 }
