16aa0c32db3f7556f793669d172162b144a4a861
Amelia Mendicino <amelia.mendicino@gmail.com>
2022-09-17 22:12:50 -0600

    Retrieve Recipient by Email
    Updated the favorites lambda to be able to grab the Recipient from the
    form event by their email address so that we'll be able to update their
    favorites later. Had to update the schema to allow IAM to have access to
    the ProfileFavorite type or it would throw an error saying "Not Authorized
    to access listRecipients on type ModelRecipientConnection."

---
+++ amplify/backend/function/favorites/src/graphql.js
@@ -0,0 +1,82 @@
+import crypto from "@aws-crypto/sha256-js";
+import { defaultProvider } from "@aws-sdk/credential-provider-node";
+import { SignatureV4 } from "@aws-sdk/signature-v4";
+import { HttpRequest } from "@aws-sdk/protocol-http";
+import { default as fetch, Request } from "node-fetch";
+
+const GRAPHQL_ENDPOINT = process.env.API_ZIPZAP_GRAPHQLAPIENDPOINTOUTPUT;
+
+const { Sha256 } = crypto;
+const AWS_REGION = process.env.AWS_REGION || "us-west-2";
+
+const recipientQuery = /* GraphQL */ `
+  query GetRecipientByEmail($filter: ModelRecipientFilterInput) {
+    listRecipients(filter: $filter) {
+      items {
+        id
+        email
+        firstName
+        lastName
+        favorites {
+          items {
+            type
+            value
+          }
+        }
+      }
+    }
+  }
+`;
+
+const graphQLQuery = async (query, variables) => {
+  const endpoint = new URL(GRAPHQL_ENDPOINT);
+  const body = { query };
+  if (variables) {
+    body.variables = variables;
+  }
+
+  const signer = new SignatureV4({
+    credentials: defaultProvider(),
+    region: AWS_REGION,
+    service: "appsync",
+    sha256: Sha256,
+  });
+
+  const requestToBeSigned = new HttpRequest({
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      host: endpoint.host,
+    },
+    hostname: endpoint.host,
+    body: JSON.stringify(body),
+    path: endpoint.pathname,
+  });
+
+  const signed = await signer.sign(requestToBeSigned);
+  const request = new Request(endpoint, signed);
+
+  const response = await fetch(request);
+  const { data = {}, errors } = await response.json();
+  if (errors) {
+    throw new Error(JSON.stringify(errors));
+  }
+  return data;
+};
+
+export const getRecipient = async (email) => {
+  let recipient;
+  const params = {
+    filter: { email: { eq: email } },
+  };
+
+  const { listRecipients } = await graphQLQuery(recipientQuery, params);
+
+  if (listRecipients?.items?.length > 0) {
+    recipient = listRecipients.items[0];
+  } else {
+    throw new Error(`No recipient found for email address ${email}`);
+  }
+
+  return recipient;
+};
--- amplify/backend/function/favorites/src/index.js
+++ amplify/backend/function/favorites/src/index.js
@@ -1,15 +1,36 @@
+/* Amplify Params - DO NOT EDIT
+	API_ZIPZAP_GRAPHQLAPIENDPOINTOUTPUT
+	API_ZIPZAP_GRAPHQLAPIIDOUTPUT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+import { getRecipient } from "./graphql.js";
+
 /**
+ * This lambda function should be invoked by Google Forms passing
+ * in an event object containing the namedValues from filling out
+ * a Favorites Survey.
+ *
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 export const handler = async (event) => {
-  console.log(`EVENT: ${JSON.stringify(event)}`);
+  const { namedValues } = event;
+  let statusCode = 200;
+
+  // Extract the email address from the form submission
+  const email = namedValues?.["Email Address"]?.[0];
+  delete namedValues?.["Email Address"];
+
+  try {
+    // Attempt to find the user from the passed in email address
+    const recipient = await getRecipient(email);
+    console.log({ recipient });
+  } catch (error) {
+    statusCode = 500;
+    console.error(`Unable to update favorites for email "${email}".`, error);
+  }
+
   return {
-    statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-    body: JSON.stringify("Hello from Lambda!"),
+    statusCode,
   };
 };
