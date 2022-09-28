36dc1778db1c77cc9a2b12d5622f15066a75dc80
Amelia Mendicino <amelia.mendicino@gmail.com>
2022-09-27 22:20:42 -0600

    Use holidaysData for Upcoming Gifts
    Using an array reduce to go through all of the holidaysData to grab the
    next three holidays. It only really works if the holidaysData is in order.
    Also updating the holidaysData with more accurate data for this year and
    early next. It's not a super efficient algorithm or anything, but it gets
    the job done.

--- src/components/GiftDashboard/GiftDashboard.js
+++ src/components/GiftDashboard/GiftDashboard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { parseISO, isAfter } from "date-fns";
 import { API, graphqlOperation } from "aws-amplify";
 import { useQuery } from "react-query";
 import { getUser } from "../../graphql/queries";
@@ -20,8 +21,67 @@ import EventRepeatIcon from "@mui/icons-material/EventRepeat";
 import TodayIcon from "@mui/icons-material/Today";
 import GiftDashboardModal from "./GiftDashboardModal";
 
-// holidays json
+// TODO: Put the holidays in the database or something. They need to be updated again next May
 const holidaysData = [
+  { name: "my day", date: "2022-05-22" },
+  { name: "National Cheese Day", date: "2022-06-04" },
+  { name: "National Donut Day", date: "2022-06-04" },
+  { name: "Father's Day", date: "2022-06-19" },
+  { name: "International Yoga Day", date: "2022-06-21" },
+
+  { name: "Canada Day", date: "2022-07-01" },
+  { name: "Independence Day", date: "2022-07-04" },
+  { name: "World Chocolate Day", date: "2022-07-07" },
+  { name: "National Video Game Day", date: "2022-07-08" },
+  { name: "National Ice Cream Day", date: "2022-07-18" },
+  { name: "Parents Day", date: "2022-07-25" },
+
+  { name: "National Friendship Day", date: "2022-08-01" },
+  { name: "International Cat's Day", date: "2022-08-08" },
+  { name: "National Women's Day", date: "2022-08-09" },
+  { name: "Back to School", date: "2022-08-20" },
+
+  { name: "Labor Day", date: "2022-09-05" },
+  { name: "Patriot Day", date: "2022-09-11" },
+  { name: "Software Developer Day", date: "2022-09-13" },
+  { name: "Batman Day", date: "2022-09-17" },
+  { name: "National IT Professionals Day", date: "2022-09-20" },
+  { name: "National Pancake Day", date: "2022-09-26" },
+
+  { name: "National Custodian Day", date: "2022-10-02" },
+  { name: "National Golf Lovers Day", date: "2022-10-04" },
+  { name: "National Taco Day", date: "2022-10-04" },
+  { name: "World Teachers day", date: "2022-10-05" },
+  { name: "World Smile Day", date: "2022-10-07" },
+  { name: "World Mental Health Day", date: "2022-10-10" },
+  { name: "Indigenous People Day", date: "2022-10-10" },
+  { name: "Colombus Day", date: "2022-10-10" },
+  { name: "National Bosses Day", date: "2022-10-16" },
+  { name: "National Paralegal Day", date: "2022-10-23" },
+  { name: "Diwali", date: "2022-10-24" },
+  { name: "National First Responders Day", date: "2022-10-28" },
+  { name: "Halloween", date: "2022-10-31" },
+
+  { name: "World Vegan Day", date: "2022-11-01" },
+  { name: "Day of the Dead", date: "2022-11-02" },
+  { name: "National Candy Day", date: "2022-11-04" },
+  { name: "National Team Manager Day", date: "2022-11-06" },
+  { name: "Veterans Day", date: "2022-11-11" },
+  { name: "World Kindness Day", date: "2022-11-13" },
+  { name: "National Hiking Day", date: "2022-11-17" },
+  { name: "Thanksgiving", date: "2022-11-24" },
+  { name: "Small Business Saturday", date: "2022-11-26" },
+  { name: "Giving Tuesday", date: "2022-11-29" },
+
+  { name: "National Bar Tender Day", date: "2022-12-02" },
+  { name: "National Cookie Day", date: "2022-12-04" },
+  { name: "National Pearl harbor Day of Remembrance", date: "2022-12-07" },
+  { name: "National Sales Person Day", date: "2022-12-09" },
+  { name: "National App Day", date: "2022-12-11" },
+  { name: "Christmas Eve", date: "2022-12-24" },
+  { name: "Christmas", date: "2022-12-25" },
+  { name: "New Years Eve", date: "2022-12-31" },
+
   { name: "New Years Day", date: "2023-01-01" },
   { name: "Martin Luther King Day", date: "2023-01-16" },
   { name: "Popcorn Day", date: "2023-01-19" },
@@ -59,65 +119,6 @@ const holidaysData = [
   { name: "Receptionist Day", date: "2023-05-10" },
   { name: "National Armed Forces Day", date: "2023-05-20" },
   { name: "National BBQ Day", date: "2023-05-16" },
-
-  { name: "my day", date: "2022-05-22" },
-  { name: "National Cheese Day", date: "2022-06-04" },
-  { name: "National Donut Day", date: "2022-06-04" },
-  { name: "Father's Day", date: "2022-06-19" },
-  { name: "International Yoga Day", date: "2022-06-21" },
-
-  { name: "Canada Day", date: "2022-07-01" },
-  { name: "Independence Day", date: "2022-07-04" },
-  { name: "World Chocolate Day", date: "2022-07-07" },
-  { name: "National Video Game Day", date: "2022-07-08" },
-  { name: "National Ice Cream Day", date: "2022-07-18" },
-  { name: "Parents Day", date: "2022-07-25" },
-
-  { name: "National Friendship Day", date: "2022-08-01" },
-  { name: "International Cat's Day", date: "2022-08-08" },
-  { name: "National Women's Day", date: "2022-08-09" },
-  { name: "Back to School", date: "2022-08-20" },
-
-  { name: "Labor Day", date: "2022-09-05" },
-  { name: "Patriot Day", date: "2022-09-11" },
-  { name: "Software Developer Day", date: "2022-09-13" },
-  { name: "Batman Day", date: "2022-09-18" },
-  { name: "National IT Professionals Day", date: "2022-09-21" },
-  { name: "National Pancake Day", date: "2022-09-26" },
-
-  { name: "World Smile Day", date: "2022-10-01" },
-  { name: "National Custodian Day", date: "2022-10-02" },
-  { name: "National Golf Lovers Day", date: "2022-10-04" },
-  { name: "National Taco Day", date: "2022-10-04" },
-  { name: "World Teachers day", date: "2022-10-05" },
-  { name: "World Mental Health Day", date: "2022-10-10" },
-  { name: "Indigenous People Day", date: "2022-10-11" },
-  { name: "Colombus Day", date: "2022-10-11" },
-  { name: "National Bosses Day", date: "2022-10-16" },
-  { name: "National Paralegal Day", date: "2022-10-23" },
-  { name: "National First Responders Day", date: "2022-10-28" },
-  { name: "Halloween", date: "2022-10-31" },
-
-  { name: "World Vegan Day", date: "2022-11-01" },
-  { name: "Day of the Dead", date: "2022-11-02" },
-  { name: "Diwali", date: "2022-11-04" },
-  { name: "National Candy Day", date: "2022-11-04" },
-  { name: "National Team Manager Day", date: "2022-11-06" },
-  { name: "Veterans Day", date: "2022-11-11" },
-  { name: "World Kindness Day", date: "2022-11-13" },
-  { name: "National Hiking Day", date: "2022-11-17" },
-  { name: "Thanksgiving", date: "2022-11-25" },
-  { name: "Small Business Saturday", date: "2022-11-27" },
-
-  { name: "Giving Tuesday", date: "2022-12-01" },
-  { name: "National Cookie Day", date: "2022-12-04" },
-  { name: "National Pearl harbor Day of Remembrance", date: "2022-12-07" },
-  { name: "National Bar Tender Day", date: "2022-12-08" },
-  { name: "National App Day", date: "2022-12-11" },
-  { name: "National Sales Person Day", date: "2022-12-13" },
-  { name: "Christmas Eve", date: "2022-12-24" },
-  { name: "Christmas", date: "2022-12-25" },
-  { name: "New Years Eve", date: "2022-12-31" },
 ];
 
 // calendar events
@@ -193,34 +194,28 @@ const calendarEvents = [
 // utilization and default gift dummy data
 const utilization = 60;
 
+// Grab the next three holidays to display on the dashboard. Assumes holidaysData is in order.
+const upcomingHolidays = holidaysData.reduce((upcomingHolidays, holiday) => {
+  if (
+    isAfter(parseISO(holiday.date), new Date()) &&
+    upcomingHolidays.length < 3
+  ) {
+    upcomingHolidays.push(holiday);
+  }
+  return upcomingHolidays;
+}, []);
 const defaultGifts = [
   {
     giftType: "Upcoming Holidays",
     gifts: [
-      {
-        name: "Labor Day",
-        date: "2022-09-05T00:00:00",
+      ...upcomingHolidays.map(({ name, date }, index) => ({
+        name,
+        date,
         icon: (
           <TodayIcon sx={{ fontSize: { lg: "2.7rem" } }} color="secondary" />
         ),
-        id: 1,
-      },
-      {
-        name: "Patriot Day",
-        date: "2022-09-11T00:00:00",
-        icon: (
-          <TodayIcon sx={{ fontSize: { lg: "2.7rem" } }} color="secondary" />
-        ),
-        id: 2,
-      },
-      {
-        name: "Software Developer Day",
-        date: "2022-09-13T00:00:00",
-        icon: (
-          <TodayIcon sx={{ fontSize: { lg: "2.7rem" } }} color="secondary" />
-        ),
-        id: 3,
-      },
+        id: index,
+      })),
     ],
     id: 1,
   },
@@ -228,35 +223,14 @@ const defaultGifts = [
   //   giftType: "Upcoming Recipient Events",
   //   gifts: [
   //     {
-  //       name: "Andrew Smith Birthday",
+  //       name: "[Name] Birthday|Anniversary",
   //       date: "2022-09-09T00:00:00",
   //       icon: (
   //         <CakeIcon sx={{ fontSize: { lg: "2.7rem" } }} color="secondary" />
   //       ),
   //       id: 1,
   //     },
-  //     {
-  //       name: "Michael Scott Anniversary",
-  //       date: "2022-09-15T00:00:00",
-  //       icon: (
-  //         <EventRepeatIcon
-  //           sx={{ fontSize: { lg: "2.7rem" } }}
-  //           color="secondary"
-  //         />
-  //       ),
-  //       id: 2,
-  //     },
-  //     {
-  //       name: "Kelsey Zaugg Anniversary",
-  //       date: "2022-09-21T00:00:00",
-  //       icon: (
-  //         <EventRepeatIcon
-  //           sx={{ fontSize: { lg: "2.7rem" } }}
-  //           color="secondary"
-  //         />
-  //       ),
-  //       id: 3,
-  //     },
+  //    ...
   //   ],
   //   id: 2,
   //  },
