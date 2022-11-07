09936b229addfce1d3d0ea3d06741e246939f902
Amelia Mendicino <amelia.mendicino@gmail.com>
2022-11-06 23:24:04 -0700

    Update Recipient Email
    Our mail-tester.com score was 7.3/10, so followed the instructions to fix
    some of the problems to increase our score to 9/10. Had to add a DMARC
    record to the DNS, add a List-Unsubscribe header, add a plain-text version,
    and remove some broken links (the links had been copied from the first
    email template made for the Cognito sign-up. Should have verified them...).
    To add the header, had to move to using the Raw Email command and brought
    in the mailcomposer library to handle all of that stuff.

---
+++ amplify/backend/function/recipientsemail/src/text_template.js
@@ -0,0 +1,9 @@
+export const text = `
+## Welcome to Zip Zap Gifts!
+
+We want to help your company send you freakin' sweet, thoughtful gifts.
+
+Copy and paste the url below into your browser to fill out the form so we can get this ball rolling!
+
+https://docs.google.com/forms/d/e/1FAIpQLSd9BRvoGLSQxzEqZxbU_DmVR8ob7W4r67tmO7ErGvz3uj-3Ow/viewform?usp=sf_link
+`;
